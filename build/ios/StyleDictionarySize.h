f05d3e8925d016b3bd7cd43b48993f2837361613
Tylen St Hilaire <28753109+tylensthilaire@users.noreply.github.com>
2022-01-18 16:43:43 +0000

    Initial build

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Tue, 18 Jan 2022 16:26:55 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
